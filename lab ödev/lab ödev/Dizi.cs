2065fd1f3be12e397aecea71d8c1c2321f9b1cdc
can bingöl <114669146+canbingol@users.noreply.github.com>
2023-11-18 01:39:51 +0300

    dizi örneği
    dizi

---
+++ lab ödev/lab ödev/Dizi.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+
+namespace lab_ödev
+{
+    internal class Dizi
+    {
+
+        public int[,] dizim = new int[8, 8];
+        public int[,] DiziAl()
+        {
+            int[,] dizi = new int[8, 8];
+
+            for (int i = 0; i < 8; i++)
+            {
+                for (int j = 0; j < 8; j++)
+                {
+                    Console.Write($"Lütfen [{i},{j}] elemanını girin: ");
+                    // metni tam sayıya çevirmeye çalışır
+                    if (int.TryParse(Console.ReadLine(), out int eleman))
+                    {
+                        dizi[i, j] = eleman;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Geçersiz bir sayı girdiniz. Lütfen tekrar deneyin.");
+                        j--;
+                    }
+                }
+            }
+            return dizi;
+        }
+
+        public void RandomDizi()
+        {
+            Random rdm = new Random();
+
+            for (int i = 0; i < 8; i++)
+            {
+                for (int j = 0; j < 8; j++)
+                {
+                    dizim[i, j] = rdm.Next(0, 10);
+                }
+            }
+        }
+
+
+
+        // girilen diziyi istenen şekle çevirir (j. elemanı (j+1)-j şekline çevirir)
+        public void DiziDuzenle(int[,] dizi)
+        {
+            for (int i = 0; i < 8; i++)
+            {
+
+                dizim[i, 0] = dizi[i, 0];
+
+                //  elemanları (j+1)-j şekline çevir 
+                for (int j = 1; j < 8; j++)
+                {
+                    dizim[i, j] = dizi[i, j] - dizi[i, j - 1];
+
+                }
+
+                //     Console.WriteLine();
+            }
+        }
+
+        // parametre olarak girilen diziyi yazdırır
+        public void DiziyiYazdir(int[,] arr)
+        {
+            Console.WriteLine("SIKIŞTIRILMIŞ DİZİ ------------");
+            for (int i = 0; i < 8; i++)
+            {
+                for (int j = 0; j < 8; j++)
+                {
+                    Console.Write(arr[i, j] + "\t");
+                }
+                Console.WriteLine();
+            }
+
+        }
+
+        // girilen dizinin her satırındaki elemanları toplayıp binaty karşılığını yazdırır
+        public void SıkıstırmaOranı(int[,] arr)
+        {
+
+            for (int i = 0; i < 8; i++)
+            {
+                string toplam = "";
+                int deger = 0;
+
+                for (int j = 0; j < 8; j++)
+                {
+                    deger += arr[i, j];
+                    toplam = Convert.ToString(deger, 2); // deger değişkeninin binary karşılıpı bulunur
+
+                }
+                Console.WriteLine(i + 1 + ". satırın sıkıştırma oranı = " + toplam);
+            }
+
+        }
+    }
+}
+
+
+
---
+++ lab ödev/lab ödev/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab_ödev
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dizi deneme = new Dizi();
+            // dizinin degeğerlerini random atamak için kullanılacak satırlar (14-16)
+            /* deneme.RandomDizi();
+              deneme.DiziDuzenle(deneme.dizim);*/
+
+            deneme.DiziDuzenle(deneme.DiziAl()); // değerler random alınacağı zaman kullanılmayacak satır
+            deneme.DiziyiYazdir(deneme.dizim);
+            deneme.SıkıstırmaOranı(deneme.dizim);
+            Console.ReadLine();
+        }
+    }
+}
